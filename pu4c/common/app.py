81753a3905a88c73d3eca29bef7e342382ac62b6
city945 <2654229893@qq.com>
2024-11-10 21:14:15 +0800

    v1.1.2   - 修复 rpc 及 setup.py 的 Bug

--- pu4c/common/app.py
+++ pu4c/common/app.py
@@ -3,7 +3,7 @@ from .config import rpc_server_ip, rpc_server_port
 def start_rpc_server():
     import rpyc, pickle
     from rpyc.utils.server import ThreadedServer
-    from pu4c.det3d.app import cloud_viewer, voxel_viewer, cloud_viewer_panels, cloud_player
+    from pu4c.det3d.app import cloud_viewer, voxel_viewer, cloud_viewer_panels, cloud_player, plot_tsne2d, plot_umap
     class RPCService(rpyc.Service):
         def exposed_cloud_viewer(self, serialized_args, serialized_kwargs):
             args, kwargs = pickle.loads(serialized_args), pickle.loads(serialized_kwargs)
@@ -17,6 +17,13 @@ def start_rpc_server():
         def exposed_cloud_player(self, serialized_args, serialized_kwargs):
             args, kwargs = pickle.loads(serialized_args), pickle.loads(serialized_kwargs)
             return pickle.dumps(cloud_player(*args, **kwargs))
+
+        def exposed_plot_tsne2d(self, serialized_args, serialized_kwargs):
+            args, kwargs = pickle.loads(serialized_args), pickle.loads(serialized_kwargs)
+            return pickle.dumps(plot_tsne2d(*args, **kwargs))
+        def exposed_plot_umap(self, serialized_args, serialized_kwargs):
+            args, kwargs = pickle.loads(serialized_args), pickle.loads(serialized_kwargs)
+            return pickle.dumps(plot_umap(*args, **kwargs))
 
     server = ThreadedServer(RPCService, port=rpc_server_port, auto_register=True)
     server.start()
